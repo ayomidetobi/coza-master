07cdefe1f7e78cd5f9cd68c05c6a221a34c96538
ayomidetobi <ayomidestobsy10@gmail.com>
2022-11-24 02:38:34 +0100

    customized commit message

--- coza/settings.py
+++ coza/settings.py
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 
 # Path where media is stored
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
